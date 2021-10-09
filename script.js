6176fd5ac8aae6bc7d226b6065128351fb25fd6b
Monica261 <desenvolvimento.midiasim@gmail.com>
2021-10-08 23:16:30 -0300

    atualizando functions

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const red = document.getElementById('redButton').onclick = clickRed;
-const yellow = document.getElementById('yellowButton').onclick = clickYellow;
-const green = document.getElementById('greenButton').onclick = clickGreen;
+const red = document.getElementById('redButton');
+const yellow = document.getElementById('yellowButton');
+const green = document.getElementById('greenButton');
 
 
 function clickRed() {
   clearLights();
   const redLight = document.getElementById('redLight').style.backgroundColor = "#c00";
 }
-
+red.addEventListener('click', clickRed);
 
 function clickYellow() {
   clearLights();
   const yellowLight = document.getElementById('yellowLight').style.backgroundColor = "#cc0";
 }
+yellow.addEventListener('click', clickYellow);
 
 function clickGreen(){
   clearLights();
   document.getElementById('greenLight').style.backgroundColor = "#0c0";
 }
+green.addEventListener('click', clickGreen);
 
 function clearLights() {
   document.getElementById('redLight').style.backgroundColor = "black";
