7e4a62ea98c949eec28c3b15d220fc30f793b559
Monica261 <desenvolvimento.midiasim@gmail.com>
2021-10-08 23:25:17 -0300

    atualizando função clickClear

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ const green = document.getElementById('greenButton');
 
 
 function clickRed() {
-  clearLights();
+  clickClear();
   const redLight = document.getElementById('redLight').style.backgroundColor = "#c00";
 }
 red.addEventListener('click', clickRed);
 
 function clickYellow() {
-  clearLights();
+  clickClear();
   const yellowLight = document.getElementById('yellowLight').style.backgroundColor = "#cc0";
 }
 yellow.addEventListener('click', clickYellow);
 
 function clickGreen(){
-  clearLights();
+  clickClear();
   document.getElementById('greenLight').style.backgroundColor = "#0c0";
 }
 green.addEventListener('click', clickGreen);
 
-function clearLights() {
+function clickClear() {
   document.getElementById('redLight').style.backgroundColor = "black";
   document.getElementById('yellowLight').style.backgroundColor = "black";
   document.getElementById('greenLight').style.backgroundColor = "black";
